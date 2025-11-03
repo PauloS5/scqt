d4e846e409921648a57b27cc967e73b3b26d7f42
PauloS5 <paulosocratesdesouza@gmail.com>
2025-11-02 21:30:15 -0500

    Cabeçalho das funções de renderização criados

---
+++ frontend/scripts/Renderers.js
@@ -0,0 +1,9 @@
+﻿// Função para renderizar as opções para gerar as questõess
+function renderOptions(count) {
+    
+}
+
+// Função para renderizar as questões
+function renderQuestions(questions) {
+
+}
