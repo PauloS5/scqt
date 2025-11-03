5a60624559b38b74de313dfe6464607f2e366103
PauloS5 <paulosocratesdesouza@gmail.com>
2025-11-02 23:15:47 -0500

    Função para teorema de pitagoras gerada

--- frontend/scripts/Generators.js
+++ frontend/scripts/Generators.js
@@ -11,3 +11,68 @@ function generateFirstDetQ() {
         }
     };
 }
+
+// Função paara gerar uma questão que envolva o teorema de pitágoras
+function generatePythagoreanTheoremQ() {
+    const pythagoreanValue = [
+        {catA: 3,  catB: 4,  hip: 5},
+        {catA: 5,  catB: 12, hip: 13},
+        {catA: 7,  catB: 24, hip: 25},
+        {catA: 8,  catB: 15, hip: 17},
+        {catA: 9,  catB: 40, hip: 41},
+        {catA: 11, catB: 60, hip: 61},
+        {catA: 12, catB: 35, hip: 37},
+        {catA: 13, catB: 84, hip: 85},
+        {catA: 16, catB: 63, hip: 65},
+        {catA: 20, catB: 21, hip: 29}
+    ];
+
+    let multiplier = Math.floor(Math.random() * 10) + 1;
+
+    if (Math.floor(Math.random() * 2)) {
+        multiplier =multiplier/10;
+    }
+
+    let values = pythagoreanValue[Math.floor(Math.random() * 10)];
+
+    values.catA *= multiplier;
+    values.catB *= multiplier;
+    values.hip  *= multiplier;
+
+    values.catA = Math.floor(values.catA * 10) / 10;
+    values.catB = Math.floor(values.catB * 10) / 10;
+    values.hip  = Math.floor(values.hip  * 10) / 10;
+
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return {
+                values: {
+                    catA: values.catA,
+                    catB: values.catB,
+                    hip: null
+                },
+                answer: values.hip
+            };
+            break;
+        case 1:
+            return {
+                values: {
+                    catA: values.catA,
+                    catB: null,
+                    hip: values.hip
+                },
+                answer: values.catB
+            };
+            break;
+        case 2:
+            return {
+                values: {
+                    catA: null,
+                    catB: values.catB,
+                    hip: values.hip
+                },
+                answer: values.catA
+            };
+            break;
+    }
+}
