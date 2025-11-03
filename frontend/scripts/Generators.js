40677b251ec8eccf339f9910e4c970579778b228
PauloS5 <paulosocratesdesouza@gmail.com>
2025-11-02 22:10:20 -0500

    Função para gerar questões de primeira determinação criada

---
+++ frontend/scripts/Generators.js
@@ -0,0 +1,7 @@
+﻿// Função para gerar uma questão para se encontrar a primeira determinação de um ângulo
+function generateFirstDetQ() {
+    const turnsCount = Math.floor(Math.random() * 10) + 1;
+    const firstDet = Math.floor(Math.random() * 360);
+
+    return turnsCount + firstDet;
+}
