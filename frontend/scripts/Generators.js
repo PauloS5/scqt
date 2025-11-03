ac4ac2a0475e71baed706b610251b1953599af7c
PauloS5 <paulosocratesdesouza@gmail.com>
2025-11-02 22:19:58 -0500

    Atualização na função

--- frontend/scripts/Generators.js
+++ frontend/scripts/Generators.js
@@ -1,7 +1,13 @@
-﻿// Função para gerar uma questão para se encontrar a primeira determinação de um ângulo
+﻿// Função para gerar uma questão para se encontrar a primeira determinação de um ângulo ou quantidade de voltas com base em graus
 function generateFirstDetQ() {
     const turnsCount = Math.floor(Math.random() * 10) + 1;
     const firstDet = Math.floor(Math.random() * 360);
 
-    return turnsCount + firstDet;
+    return {
+        value: turnsCount + firstDet,
+        answers: {
+            turnsCount: turnsCount,
+            firstDet: firstDet
+        }
+    };
 }
