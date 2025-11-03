3e52b7ad0ecfc70e49fb6d7c11d6cf3b47f960c1
PauloS5 <paulosocratesdesouza@gmail.com>
2025-11-03 00:43:14 -0500

    Ajustes para implementar funcionalidades

--- frontend/scripts/Renderers.js
+++ frontend/scripts/Renderers.js
@@ -6,25 +6,27 @@ function renderOptions(count) {
 
     for (let i = 1; i <= count; i++) {
         const div = document.createElement("div");
-        div.className = "text-white";
+        div.className = "text-white flex justify-between items-center";
 
         div.innerHTML = `
             <span class="font-semibold">QUESTÃO ${i}</span>
-            <select
-                class="bg-slate-800 hover:bg-slate-700 text-white cursor-pointer mt-2 p-1 rounded mx-2"
-                id="theme-${i}">
+            <div>
+                <select
+                    class="bg-slate-800 hover:bg-slate-700 text-white cursor-pointer mt-2 p-1 rounded mx-2"
+                    id="theme-${i}">
 
-                <option value="theme1">Primeira Determinação</option>
-                <option value="theme2">Teorema de Pitágoras</option>
-                <option value="theme3">Relação Trigométrica</option>
-            </select>
-            <select
-                class="bg-slate-800 hover:bg-slate-700 text-white cursor-pointer mt-2 p-1 rounded mx-2"
-                id="style-${i}">
+                    <option value="theme1">Primeira Determinação</option>
+                    <option value="theme2">Teorema de Pitágoras</option>
+                    <option value="theme3">Relação Trigométrica</option>
+                </select>
+                <select
+                    class="bg-slate-800 hover:bg-slate-700 text-white cursor-pointer mt-2 p-1 rounded mx-2"
+                    id="style-${i}">
 
-                <option value="style1">Demonstrativa</option>
-                <option value="style2">Objetiva</option>
-            </select>
+                    <option value="style1">Demonstrativa</option>
+                    <option value="style2">Objetiva</option>
+                </select>
+            <div>
         `;
 
         container.appendChild(div);
